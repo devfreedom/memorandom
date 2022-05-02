b2b10596dff863741ec8bd856c467c6148aebaac
owner <devfreedomHQ@protonmail.com>
2022-05-02 16:01:15 +0900

    Layout reset

---
+++ README.md
@@ -0,0 +1,7 @@
+# devFreedom.memoRandom()
+
+devfreedom's personal blog.
+
+Powered by [11ty](https://www.11ty.dev/) and [TailwindCSS](https://tailwindcss.com/).
+
+Forked from Vredeburg by [Dafiul Haq](https://github.com/dafiulh/vredeburg).
---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'devFreedom.memoRandom()',
+  description: "devfreedom personal blog",
+  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  // your site url without trailing slash
+  url: 'https://memorandom.netlify.app',
+  // how many posts you want to show for each page
+  paginate: 6
+  // if you want to add disqus to your site
+  // disqusShortname: "your-shortname"
+};
