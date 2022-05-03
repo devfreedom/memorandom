3704320a5fe99d2db89cab7d3734d0ee139016e6
owner <devfreedomHQ@protonmail.com>
2022-05-03 19:43:20 +0900

    Adjust hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,38 +16,42 @@ module.exports = {
       lg: '1024px',
       xl: '1280px'
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: {
-        100: '#f7fafc',
-        200: '#edf2f7',
-        300: '#e2e8f0',
-        400: '#cbd5e0',
-        500: '#a0aec0',
-        600: '#718096',
-        700: '#4a5568',
-        800: '#2d3748',
-        900: '#1a202c'
-      },
-      green: colors.green,
-      red: colors.red,
-      blue: colors.blue,
-      slate: colors.slate,
-      orange: colors.orange,
-      teal: colors.teal,
-      amber: colors.amber,
-      emerald: colors.emerald,
-      lime: colors.lime,
-      yellow: colors.yellow,
-      cyan: colors.cyan,
-      purple: colors.purple,
-      indigo: colors.indigo,
-      skyblue: colors.skyblue,
-    },
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c'
+        },
+        green: colors.green,
+        red: colors.red,
+        blue: colors.blue,
+        slate: colors.slate,
+        orange: colors.orange,
+        teal: colors.teal,
+        amber: colors.amber,
+        emerald: colors.emerald,
+        lime: colors.lime,
+        yellow: colors.yellow,
+        cyan: colors.cyan,
+        purple: colors.purple,
+        indigo: colors.indigo,
+        skyblue: colors.skyblue,
+      },
+      height: {
+        112: '28rem',
+        128: '32rem'
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
